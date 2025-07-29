a962a73b52bbbdcc361b1e1cbf1f4fd05906f6ec
shubhsrii <srivastava.shubh3008@gmail.com>
2025-07-29 22:30:14 +0530

    fixed build error

--- EbiditBusinessLandingFE/src/app/loading.jsx
+++ EbiditBusinessLandingFE/src/app/loading.jsx
@@ -1,24 +1,36 @@
 'use client';
-import React from 'react'
+
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import elogo from '../EnquiryForm/images/e-bid_app_1_red.png'
+// import elogo from '../EnquiryForm/images/e-bid_app_1_red.png'
 
 const loading = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsFullscreen(window.innerHeight === screen.height);
+    }
+  }, []);
+
   return (
-    <div className={`${
-        fullscreen ? 'h-screen' : 'h-full'
-      } w-full flex flex-col items-center justify-center bg-white z-50`}>
+    <div
+      className={`${
+        isFullscreen ? 'h-screen' : 'h-full'
+      } w-full flex flex-col items-center justify-center bg-white z-50`}
+    >
       <Image
-        src={elogo} 
+        src="/e-bid_app_1_red.png"
         alt="Ebidit Logo"
         width={80}
         height={80}
         className="animate-bounce"
       />
-      <p className="mt-4 text-gray-600 text-lg animate-pulse">{text}</p>
+      <p className="mt-4 text-gray-600 text-lg animate-pulse">
+        Loading...
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default loading
-
+export default loading;
